e84d10caad5cd16e9d19b3c0ccfa2624ea1da4ec
judson <judeezeh2012@gmail.com>
2023-01-25 09:07:44 -0800

    model deployment

--- app.py
+++ app.py
@@ -14,8 +14,13 @@ def greet(name:str):
 
 def prediction(area_mean:float, concavity_mean:float, concave points_mean:float, radius_worst:float, perimeter_worst:float, area_worst:float, concave points_worst:float, fractal_dimension_worst:float, compactness_worst:float, concave points_se:float, fractal_dimension_mean:float):
     model = joblib.load('Model.pkl')
-    make_prediction = model.predict([[area_mean, concavity_mean, concave points_mean, radius_worst, perimeter_worst,
-area_worst, concave points_worst, fractal_dimension_worst, compactness_worst, concave points_se,
-fractal_dimension_mean]])
+    make_prediction = model.predict([[area_mean, concavity_mean, concave points_mean, radius_worst, perimeter_worst,area_worst, concave points_worst, fractal_dimension_worst, compactness_worst, concave points_se,fractal_dimension_mean]])
+    result = make_prediction[0]
+    
+    if result == 0:
+        return {"This patient is breast cancer free."}
+    else:
+        return {'This patient is having breast cancer'}
+
 
 
